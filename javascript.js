7c69003d551cd62b4547aeb5cd7115513171ffd5
Ben Ramins <amrabennis@gmail.com>
2020-07-28 02:18:10 +0200

    HTML/JS/CSS/German text files
    First contribution from @BenRamins. Hard coded the examples for the user story. Please see comment in JS file.

---
+++ javascript.js
@@ -0,0 +1,73 @@
+
+// @BenRamins: there has to be an easier way to do this, but my JavaScript skills are still very low. This is what I figured out so far
+
+
+var correctButton3 = document.getElementById("correct3")
+
+correctButton3.addEventListener("click", function(){
+    correctButton3.textContent = "Correct!";
+    correctButton3.style.backgroundColor = "green";
+})
+
+var wrongButton1 = document.getElementById("wrong1")
+
+wrongButton1.addEventListener("click", function(){
+    wrongButton1.textContent = "Wrong!";
+    wrongButton1.style.backgroundColor = "red";
+})
+
+var wrongButton2 = document.getElementById("wrong2")
+
+wrongButton2.addEventListener("click", function(){
+    wrongButton2.textContent = "Wrong!";
+    wrongButton2.style.backgroundColor = "red";
+})
+
+// 
+
+var correctButton6 = document.getElementById("correct6")
+
+correctButton6.addEventListener("click", function(){
+    correctButton6.textContent = "Correct!";
+    correctButton6.style.backgroundColor = "green";
+})
+
+var wrongButton4 = document.getElementById("wrong4")
+
+wrongButton4.addEventListener("click", function(){
+    wrongButton4.textContent = "Wrong!";
+    wrongButton4.style.backgroundColor = "red";
+})
+
+var wrongButton5 = document.getElementById("wrong5")
+
+wrongButton5.addEventListener("click", function(){
+    wrongButton5.textContent = "Wrong!";
+    wrongButton5.style.backgroundColor = "red";
+})
+
+// 
+
+var correctButton9 = document.getElementById("correct9")
+
+correctButton9.addEventListener("click", function(){
+    correctButton9.textContent = "Correct!";
+    correctButton9.style.backgroundColor = "green";
+})
+
+var wrongButton7 = document.getElementById("wrong7")
+
+wrongButton7.addEventListener("click", function(){
+    wrongButton7.textContent = "Wrong!";
+    wrongButton7.style.backgroundColor = "red";
+})
+
+var wrongButton8 = document.getElementById("wrong8")
+
+wrongButton8.addEventListener("click", function(){
+    wrongButton8.textContent = "Wrong!";
+    wrongButton8.style.backgroundColor = "red";
+})
+
+
+
