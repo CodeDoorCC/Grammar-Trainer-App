deb88f5762275077cdbf3bffd0f7c5d0270190a1
Ben Ramins <amrabennis@gmail.com>
2020-07-29 01:05:39 +0200

    Multiple changes
    HTML - added German language attribute for the main section for accessibility + added new words
    
    CSS - implemented Flexbox with wrapping
    
    JS - added new buttons functionalities to go with some of the new words
    
    German.txt - added new words with articles

--- javascript.js
+++ javascript.js
@@ -69,5 +69,96 @@ wrongButton8.addEventListener("click", function(){
     wrongButton8.style.backgroundColor = "red";
 })
 
+// 
 
+var correctButton12 = document.getElementById("correct12")
 
+correctButton12.addEventListener("click", function(){
+    correctButton12.textContent = "Correct!";
+    correctButton12.style.backgroundColor = "green";
+})
+
+var wrongButton10 = document.getElementById("wrong10")
+
+wrongButton10.addEventListener("click", function(){
+    wrongButton10.textContent = "Wrong!";
+    wrongButton10.style.backgroundColor = "red";
+})
+
+var wrongButton11 = document.getElementById("wrong11")
+
+wrongButton11.addEventListener("click", function(){
+    wrongButton11.textContent = "Wrong!";
+    wrongButton11.style.backgroundColor = "red";
+})
+
+// 
+
+var correctButton15 = document.getElementById("correct15")
+
+correctButton15.addEventListener("click", function(){
+    correctButton15.textContent = "Correct!";
+    correctButton15.style.backgroundColor = "green";
+})
+
+var wrongButton13 = document.getElementById("wrong13")
+
+wrongButton13.addEventListener("click", function(){
+    wrongButton13.textContent = "Wrong!";
+    wrongButton13.style.backgroundColor = "red";
+})
+
+var wrongButton14 = document.getElementById("wrong14")
+
+wrongButton14.addEventListener("click", function(){
+    wrongButton14.textContent = "Wrong!";
+    wrongButton14.style.backgroundColor = "red";
+})
+
+// 
+
+var correctButton18 = document.getElementById("correct18")
+
+correctButton18.addEventListener("click", function(){
+    correctButton18.textContent = "Correct!";
+    correctButton18.style.backgroundColor = "green";
+})
+
+var wrongButton16 = document.getElementById("wrong16")
+
+wrongButton16.addEventListener("click", function(){
+    wrongButton16.textContent = "Wrong!";
+    wrongButton16.style.backgroundColor = "red";
+})
+
+var wrongButton17 = document.getElementById("wrong17")
+
+wrongButton17.addEventListener("click", function(){
+    wrongButton17.textContent = "Wrong!";
+    wrongButton17.style.backgroundColor = "red";
+})
+
+// 
+
+var correctButton21 = document.getElementById("correct21")
+
+correctButton21.addEventListener("click", function(){
+    correctButton21.textContent = "Correct!";
+    correctButton21.style.backgroundColor = "green";
+})
+
+var wrongButton19 = document.getElementById("wrong19")
+
+wrongButton19.addEventListener("click", function(){
+    wrongButton19.textContent = "Wrong!";
+    wrongButton19.style.backgroundColor = "red";
+})
+
+var wrongButton20 = document.getElementById("wrong20")
+
+wrongButton20.addEventListener("click", function(){
+    wrongButton20.textContent = "Wrong!";
+    wrongButton20.style.backgroundColor = "red";
+})
+
+// To have all words functional in hard-coding mode, need to add all the new buttons. 
