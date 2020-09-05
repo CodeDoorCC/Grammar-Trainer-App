0820245f637ed429771a0c06b7f8f16458c7a4d2
Gregfanyan <grigorfanyan@gmail.com>
2020-09-05 14:58:53 +0200

    fixed pull request errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,15 @@
-import React from "react";
+import React from 'react';
 
 export default () => {
-  return (
-    <div>
-      <header className="header">
-        <h1>
-          {" "}
-          <i
-            className="em em-de"
-            aria-role="presentation"
-            aria-label="Germany Flag"
-          ></i>{" "}
-          German Grammar Trainer{" "}
-          <i
-            className="em em-de"
-            aria-role="presentation"
-            aria-label="Germany Flag"
-          ></i>{" "}
-        </h1>
-        <h3>Practice your German skills!</h3>
-      </header>
-    </div>
-  );
-};
+
+    return(
+
+        <div>
+         <header className='header'>
+            <h1> <i className ="em em-de" aria-role="presentation" aria-label="Germany Flag"></i> German Grammar Trainer <i className="em em-de" aria-role="presentation" aria-label="Germany Flag"></i> </h1>
+            <h3>Practice your German skills!</h3>
+          </header>
+        </div>
+    )
+
+}
